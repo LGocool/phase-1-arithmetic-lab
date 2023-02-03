40f9d340afbffc60e537d0bfbf4f540adc90c37f
LGocool <67392650+LGocool@users.noreply.github.com>
2023-02-03 12:35:18 -0500

    passed all test in arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 62 ;
+const num2 = 1 ;
+const multiply = num1 * num2 ;
+console.log(multiply);
 
-// Write your code here
+const random = Math.ceil (Math.random()*10);
+console.log(random);
+
+const num3 = 49 ;
+const num4 = 5 ;
+const mod =  num3 % num4;
+console.log(mod);
+
+const max = Math.max(0,1,20);
+console.log(max);
